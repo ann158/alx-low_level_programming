470046f2514933d824f052e1d128a6812e83b9e6
ann158 <annekwamboka57@gmail.com>
2023-02-21 23:52:52 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entries
+ * should be followed by a new line
+ * The programme should return 0
+ * Return: always 0
+ *
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ann[] = "_putchar";
+
+	int c;
+	
+	for (c = 0; c <= 8; c++)
+	{
+		_putchar(ann[c]);
+	}
 	_putchar('\n');
 	return (0);
 }
